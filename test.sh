fd51f2a11eecc00a6b6912858c17f0fa1166da7e
Michael Darden <mdarden@us.ibm.com>
2020-05-11 21:14:44 -0500

    initial commit

---
+++ test.sh
@@ -0,0 +1,5 @@
+<<COMMENT
+curl -u "apikey:1RS_Y5xSKW30OZs7s6WdRwPx3DMlBjw_Der9UagIFjNK" "https://api.us-east.assistant.watson.cloud.ibm.com/instances/30eaca77-fb52-43f5-b3b7-d77071997a91/v1/workspaces?version=2020-04-01"
+COMMENT
+
+curl -u "apikey":"3uHipFbsEux_sDg_8ByTNvOX00y5CrIr2KUI5CGTxatp" "https://gateway.watsonplatform.net/discovery/api/v1/environments/system/collections?version=2019-04-30"
